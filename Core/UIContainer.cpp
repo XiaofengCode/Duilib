82df75da8cf7e01bbfd06a02ee4ce227e362e030
XiaoFeng <909049038@qq.com>
2019-11-11 17:20:21 +0800

    优化触摸屏拖拽

--- Core/UIContainer.cpp
+++ Core/UIContainer.cpp
@@ -243,27 +243,31 @@ namespace DuiLib
 		}
 		if (event.Type == UIEVENT_GESTURE)
 		{
-			PGESTUREINFO pGi = (PGESTUREINFO)event.lParam;
-			if (!pGi || pGi->dwID != GID_PAN)
+			if ((m_pVerticalScrollBar != NULL && m_pVerticalScrollBar->IsVisible() && m_pVerticalScrollBar->IsEnabled())
+				|| (m_pHorizontalScrollBar != NULL && m_pHorizontalScrollBar->IsVisible() && m_pHorizontalScrollBar->IsEnabled()))
 			{
+				PGESTUREINFO pGi = (PGESTUREINFO)event.lParam;
+				if (!pGi || pGi->dwID != GID_PAN)
+				{
+					return;
+				}
+				static POINT ptLastMouse;
+				if (pGi->dwFlags & GF_BEGIN)
+				{
+					ptLastMouse = event.ptMouse;
+				}
+				int nDeltaX = event.ptMouse.x - ptLastMouse.x;
+				int nDeltaY = event.ptMouse.y - ptLastMouse.y;
+				ptLastMouse = event.ptMouse;
+				if (nDeltaX | nDeltaY)
+				{
+					SIZE sz = GetScrollPos();
+					sz.cx -= nDeltaX;
+					sz.cy -= nDeltaY;
+					SetScrollPos(sz);
+				}
 				return;
 			}
-			static POINT ptLastMouse;
-			if (pGi->dwFlags & GF_BEGIN)
-			{
-				ptLastMouse = event.ptMouse;
-			}
-			int nDeltaX = event.ptMouse.x - ptLastMouse.x;
-			int nDeltaY = event.ptMouse.y - ptLastMouse.y;
-			ptLastMouse = event.ptMouse;
-			if (nDeltaX | nDeltaY)
-			{
-				SIZE sz = GetScrollPos();
-				sz.cx -= nDeltaX;
-				sz.cy -= nDeltaY;
-				SetScrollPos(sz);
-			}
-			return;
 		}
 		if( m_pVerticalScrollBar != NULL && m_pVerticalScrollBar->IsVisible() && m_pVerticalScrollBar->IsEnabled() )
 		{
